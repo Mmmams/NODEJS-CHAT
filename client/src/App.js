3ff9a7c1fd8a08ab090496919d77034aa37d86f5
Sergei <sergei@wsa-100-73a.icx.local>
2021-04-05 15:49:53 +0300

    feat: added react-touter-dom; added context provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { UserContext } from "./UserContext";
+import Chat from "./components/chat/chat";
+import Home from "./components/home/home";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{ user, setUser }}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="chat" component={Chat} />
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
   );
 }
 
